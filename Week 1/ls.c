b11c6d6981e5529d1550ef3e100d0955031d7e20
Mrinalini Nair Ani <91671136+hacksh4w@users.noreply.github.com>
2023-07-04 09:56:34 +0530

    Update ls.c

--- Week 1/ls.c
+++ Week 1/ls.c
@@ -1,49 +1,20 @@
+// Simulation of ls unix command
 #include <stdio.h>
-#include <dirent.h>
 #include <stdlib.h>
-#include <string.h>
-
-#define FILE 8
-#define FOLDER 84
-void printFiles(char path[100], int tabCount)
-{
-    struct dirent *dptr;
-    DIR *dirp;
-    if ((dirp = opendir(path)) == NULL)
-    {
-    }
-    //printf("%s %d \n", path, tabCount);
-    
-    while (dptr = readdir(dirp))
-    {
-        if (strcmp(dptr->d_name, ".") == 0)
-        {
-            continue;
-        }
-        if (strcmp(dptr->d_name, "..") == 0)
-        {
-            continue;
-        }
-        for(int i=0; i<tabCount; i++)
-    	    printf("\t");
-        printf("%s", dptr->d_name);
-        if (dptr->d_type == FILE)
-        {
-            printf("\n");
-        }
-        else
-        {
-            printf("/\n");
-            strcat(path, "/");
-            strcat(path, dptr->d_name);
-            printFiles(path, tabCount+1);
-        }
-    }
+#include <dirent.h>
+void main(){
+	char dirname[10];
+	DIR *dirp;
+	struct dirent *d;
+	printf("Enter directory name: ");
+	scanf("%s",dirname);
+	dirp = opendir(dirname);
+	if (dirp == NULL){
+		perror("Cannot find directory");
+		exit(-1);
+	}
+	while (d = readdir(dirp))
+		printf("%s\n",d->d_name);
 }
 
-int main()
-{
-    char rootPath[100] = ".";
-    printFiles(rootPath, 0);
-    return 0;
-}
+
